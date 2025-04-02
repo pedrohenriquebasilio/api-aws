33e8df9dd5d06beec8657595d5cdc0a902f9d527
Pedro Basilio <phbasilio6903@gmail.com>
2025-04-01 22:13:21 -0300

    Feature: add a docker para rodar o postgreesql

---
+++ api/src/main/resources/db/migration/V1__create-event-table.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+
+CREATE TABLE event(
+    id UUID get_random_uuid() PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    img_url VARCHAR(100) NOT NULL,
+    event_url VARCHAR(100) NOT NULL,
+    date TIMESTAMP NOT NULL,
+    remote BOOLEAN NOT NULL,
+
+);
