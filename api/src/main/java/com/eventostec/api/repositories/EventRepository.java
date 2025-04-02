0e28c65d08205b5bac7f17bff0d46702976c4818
Pedro Basilio <phbasilio6903@gmail.com>
2025-04-02 10:23:06 -0300

    feat: Adicionei o Repository

--- api/src/main/java/com/eventostec/api/repositories/EventRepository.java
+++ api/src/main/java/com/eventostec/api/repositories/EventRepository.java
@@ -1,6 +1,6 @@
 package com.eventostec.api.repositories;
 
-import com.eventostec.api.domain.Event;
+import com.eventostec.api.domain.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
